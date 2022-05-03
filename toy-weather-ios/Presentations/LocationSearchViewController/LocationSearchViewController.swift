70611a3c30bbe2c7072fd74acfa1f1e007defe15
sungyeopTW <danielchoo@twinny.ai>
2022-05-03 16:12:39 +0900

    [RPD-3104] Add LocationSearchTableView

--- toy-weather-ios/Presentations/LocationSearchViewController/LocationSearchViewController.swift
+++ toy-weather-ios/Presentations/LocationSearchViewController/LocationSearchViewController.swift
@@ -12,29 +12,63 @@ import Then
 
 class LocationSearchViewController: UIViewController {
     
+    private var mockCellCount = 0;
+    
+    private let locationSearchTableView = LocationSearchTableView()
+    
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // NavigationController setting
-        self.setNavigationController()
-        
+        self.setNavigationController() /// NavigationController setting
+        self.setLocationSearchTableView() /// LocationSearchTableView setting
     }
     
     
     // MARK: - Methods
     
-    func setNavigationController() {
+    private func setNavigationController() {
         self.navigationController?.navigationBar.prefersLargeTitles = true /// Large Title
-        self.navigationItem.title = "오늘의 날씨 정보 👩🏻‍💼"
+        self.navigationItem.title = "오늘의 날씨 정보 🧑🏻‍💼"
         
         // UISearchController
         let searchController = UISearchController().then({
             $0.searchBar.placeholder = "지역을 입력하세요 🗺"
+            $0.obscuresBackgroundDuringPresentation = false
+            $0.searchBar.delegate = self
         })
         self.navigationItem.searchController = searchController
+        
     }
     
+    private func setLocationSearchTableView() {
+        self.view.addSubview(locationSearchTableView)
+        locationSearchTableView.snp.makeConstraints({
+            $0.edges.equalToSuperview()
+        })
+        
+    }
+    
+}
+
+
+// MARK: - LocationSearchBarDelegate
+
+extension LocationSearchViewController: UISearchBarDelegate {
+    
+    // searchBar에 입력 시작 시 locationSearchTableView 보이게
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.locationSearchTableView.tableViewCellCount = 10 // row count 변경
+        self.locationSearchTableView.reloadData() // reload
+        self.locationSearchTableView.isHidden = false // 보이게
+    }
+    
+    // searchBar에 입력 종료 시 locationSearchTableView 숨김
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        self.locationSearchTableView.tableViewCellCount = 0
+        self.locationSearchTableView.reloadData()
+        self.locationSearchTableView.isHidden = true
+    }
 }
